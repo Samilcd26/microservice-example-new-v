33e8704e27385aa9d51cde6a29ca53e246158f32
samilcd26 <samilcd.26@outlook.com>
2023-07-04 15:53:17 +0300

    This project is microservice-example new version

---
+++ currency-exchange-service/src/main/java/com/example/microservice/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,44 @@
+package com.example.microservice.currencyexchangeservice;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to){
+
+
+        logger.info("retrieveExchangeValue called with {} to {}",from,to);
+
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange==null){
+            throw new  RuntimeException("Unable to find data from:" + from);
+        }
+
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+
+        currencyExchange.setEnvironment(port);
+    return currencyExchange;
+    }
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values ( 10001,'USD','TL',65,'' );
+INSERT INTO currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values ( 10002,'EUR','TL',65,'' );
+INSERT INTO currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values ( 10003,'AUD','TL',65,'' );
